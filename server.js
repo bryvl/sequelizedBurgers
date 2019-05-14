e7d9b3c97bcf0e203a438179c6449a0dbc99e490
bryvl <bryan.val93@gmail.com>
2019-05-14 14:16:30 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require("express");
+var db = require('./models');
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Import routes and give the server access to them.
+require("./controllers/burgersController.js")(app);
+
+// app.use(routes);
+
+db.sequelize.sync({force: true}).then(function(){
+  app.listen(PORT, function() {
+    console.log("Listening on port:%s", PORT);
+  });
+});
+
+
